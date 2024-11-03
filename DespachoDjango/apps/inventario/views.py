71d178ba15fc5ed17404243ea7652714a0346849
MoisesMunoz <mo.munoz@duocuc.cl>
2024-11-03 16:11:33 -0300

    fixes #17: Hecho

--- DespachoDjango/apps/inventario/views.py
+++ DespachoDjango/apps/inventario/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import ProductForm
+from .models import Product
 
 # Create your views here.
+def editar_producto(request, id):
+    producto = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=producto)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductForm(instance=producto)
+    return render(request, 'actualizar_producto.html', {'form': form})
