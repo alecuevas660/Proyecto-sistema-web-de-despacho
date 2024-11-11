3cb6ab607b6eb93ab027b330e5637e3191d753d2
CETECOM <cetecom_conce@duoc.cl>
2024-11-11 09:44:30 -0300

    Desarrollo pagina reportes
    Se agrego la pagina reportes junto con lo solicitado como paginación, tabla de reportes y buscador

--- DespachoDjango/core/urls.py
+++ DespachoDjango/core/urls.py
@@ -4,8 +4,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from apps.home.views import HomeView
+from . import views
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
+    path('reportes/', views.reporte_view, name='reportes'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
